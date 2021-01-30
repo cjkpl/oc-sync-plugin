4b4be26804def0ae44aabd07f68d3f94fd31268a
Grzegorz <gk@cjk.pl>
2021-01-30 13:53:55 +0100

    Added assertions to ensure pivot contents are updated and retrieved after update

--- tests/models/PivotTest.php
+++ tests/models/PivotTest.php
@@ -6,27 +6,11 @@ use PluginTestCase;
 
 class PivotTest extends PluginTestCase
 {
-    public function setUp(): void
+    public function testTwoConsecutiveSyncCalls()
     {
-        parent::setUp();
-
         // create one lesson and one student
         $s = Student::create([]);
         $l = Lesson::create([]);
-    }
-
-    public function tearDown(): void
-    {
-        Student::truncate();
-        Lesson::truncate();
-
-        parent::tearDown();
-    }
-
-    public function testPivot()
-    {
-        $s = Student::first();
-        $l = Lesson::first();
 
         $s->lessons()->sync([$l->id]);
         $s->lessons()->sync([$l->id]);
@@ -34,4 +18,34 @@ class PivotTest extends PluginTestCase
         //expect one lesson attached to student
         self::assertEquals(1, $s->lessons()->count());
     }
+
+    public function testUpdatePivotDataBetweenSyncCalls()
+    {
+        // create one lesson and one student
+        $s = Student::create([]);
+        $l = Lesson::create([]);
+
+        $sid = $s->id;
+
+        $s->lessons()->sync([$l->id => ['visits' => 1]]);
+        $v = $s->lessons()->where('id',$l->id)->first()->pivot->visits;
+
+        // increase a value on intermediary table
+        $new_v = intval($v) + 1;
+
+        // update value on intermediary table
+        $s->lessons()->sync([$l->id => ['visits' => $new_v]]);
+
+        // when we access the relationship from the "other end", it works
+        $v = $l->students()->where('id',$s->id)->first()->pivot->visits;
+        self::assertEquals($new_v, $v, 'expect \'visits\' column contain 2');
+
+        // ... but when we try to re-use the original model, it fails
+        // this fails $s->fresh('lessons');
+        // this also fails $s = Student::find($sid);
+        
+        $v = $s->lessons()->where('id',$l->id)->first()->pivot->visits;
+        self::assertEquals($new_v, $v, 'expect \'visits\' column contain 2');
+    }
+
 }
